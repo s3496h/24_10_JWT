5bff394cdd3e0d08af0229216e1e9a78248ef485
s3496h <s3496h@gmail.com>
2024-10-28 13:08:41 +0900

    프로젝트 세팅

---
+++ src/main/java/org/koreait/exit/jwt_24_10/Jwt2410Application.java
@@ -0,0 +1,13 @@
+package org.koreait.exit.jwt_24_10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jwt2410Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jwt2410Application.class, args);
+    }
+
+}
