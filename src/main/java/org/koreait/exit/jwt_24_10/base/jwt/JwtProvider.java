eca4389ff188378008ac8d725e20087a67401317
s3496h <s3496h@gmail.com>
2024-10-29 11:30:14 +0900

    accessToken 생성

--- src/main/java/org/koreait/exit/jwt_24_10/base/jwt/JwtProvider.java
+++ src/main/java/org/koreait/exit/jwt_24_10/base/jwt/JwtProvider.java
@@ -1,10 +1,16 @@
 package org.koreait.exit.jwt_24_10.base.jwt;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.koreait.exit.jwt_24_10.util.Ut;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import javax.crypto.SecretKey;
 import java.util.Base64;
+import java.util.Date;
+import java.util.Map;
+
 @Component
 public class JwtProvider {
     private SecretKey cachedSecretKey;
@@ -17,5 +23,14 @@ public class JwtProvider {
     public SecretKey getSecretKey() {
         if (cachedSecretKey == null) cachedSecretKey = _getSecretKey();
         return cachedSecretKey;
+    }
+    public String getToken(Map<String, Object> claims, int seconds) {
+        long now = new Date().getTime();
+        Date accessTokenExpiresIn = new Date(now + 1000L * seconds);
+        return Jwts.builder()
+                .claim("body", Ut.json.toStr(claims))
+                .setExpiration(accessTokenExpiresIn)
+                .signWith(getSecretKey(), SignatureAlgorithm.HS512)
+                .compact();
     }
 }
